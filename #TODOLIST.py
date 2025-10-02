26b674b49e78e956834130bc98c3033f1dc46a7c
ardasrms <ardasararmis06@gmail.com>
2025-10-02 10:12:44 +0300

    Add files via upload

---
+++ #TODOLIST.py
@@ -0,0 +1,10 @@
+# Bu program basit bir yapılacaklar listesi uygular
+tasks = []  # Görevleri sakladığımız liste
+
+def show_menu():
+    # Kullanıcıya menüyü gösteren fonksiyon
+    print("\n--- Yapılacaklar Listesi ---")
+    print("1. Görev ekle")
+    print("2. Görevleri listele")
+    print("3. Görev sil")
+    print("4. Çıkış")
